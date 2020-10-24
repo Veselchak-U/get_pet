2043c92bfd2c24eaa89aabee9327b6a252e5bd65
Dmitriy Usatov <usatov.dv@gmail.com>
2020-10-24 12:58:14 +0300

    appBar complete

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
 
+  final notificationCount = 2;
+  final userAvatarImage =
+      'https://images.unsplash.com/photo-1602773890240-87ce74fc752e?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80';
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -10,27 +14,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final _backgroundColor = Colors.white;
   final _iconColor = Colors.black54;
-  var _notificationCount = 7;
+  HomeScreen _data;
+
+  @override
+  void initState() {
+    _data = widget;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: _appBar(),
-          body: Container(
-            color: _backgroundColor,
-            child: Center(
-              child: Text(
-                'HomeScreen',
-                style: Theme.of(context).textTheme.headline4,
+    return Scaffold(
+      appBar: _appBar(),
+      body: Container(
+        color: _backgroundColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _header(),
+              Center(
+                child: Text(
+                  'HomeScreen',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
               ),
-            ),
+            ],
           ),
         ),
-        Align(
-          alignment: Alignment(0.96, -0.927),
-          child: _userProfile(),
+      ),
+    );
+  }
+
+  Widget _header() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'HomeScreen',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        Text(
+          'HomeScreen',
+          style: Theme.of(context).textTheme.headline4,
         ),
       ],
     );
@@ -38,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget _appBar() {
     return AppBar(
-      backgroundColor: _backgroundColor, //Colors.black26,
+      backgroundColor: _backgroundColor,
       elevation: 0.0,
       leading: IconButton(
         tooltip: 'Something',
@@ -49,64 +74,57 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () {},
       ),
       actions: [
-        Stack(
-          alignment: Alignment(0.75, -0.5),
-          children: [
-            Center(
-              child: IconButton(
-                tooltip: 'You have new notification',
-                icon: Icon(
-                  Icons.notifications_none,
-                  color: _iconColor,
-                ),
-                onPressed: () {},
-              ),
-            ),
-            CircleAvatar(
-              radius: 10.0,
-              backgroundColor: _backgroundColor,
-              child: CircleAvatar(
-                radius: 8.0,
-                backgroundColor: Colors.orange,
-                child: Text(
-                  '$_notificationCount',
-                  style: TextStyle(color: Colors.white, fontSize: 12),
-                ),
-              ),
-            ),
-          ],
-        ),
-        // _userProfile(),
-        SizedBox(width: 70),
-        // Material(
-        //   elevation: 8.0,
-        //   shape: CircleBorder(),
-        //   // clipBehavior: Clip.antiAlias,
-        //   child: CircleAvatar(
-        //     radius: 28.0,
-        //     backgroundColor: _backgroundColor,
-        //     child: CircleAvatar(
-        //       radius: 26.0,
-        //       backgroundColor: Colors.orange,
-        //       backgroundImage: NetworkImage('https://images.unsplash.com/photo-1602773890240-87ce74fc752e?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80'),
-        //     ),
-        //   ),
-        // ),
+        (_data.notificationCount != null && _data.notificationCount > 0)
+            ? Stack(
+                alignment: Alignment(0.8, -0.5),
+                children: [
+                  Center(
+                    child: IconButton(
+                      tooltip:
+                          'You have ${_data.notificationCount} new notification(s)',
+                      icon: Icon(
+                        Icons.notifications_none,
+                        color: _iconColor,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 10.0,
+                    backgroundColor: _backgroundColor,
+                    child: CircleAvatar(
+                      radius: 8.0,
+                      backgroundColor: Colors.orange,
+                      child: Text(
+                        '${_data.notificationCount}',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : SizedBox.shrink(),
+        _userProfile(),
       ],
     );
   }
 
   Widget _userProfile() {
-    return FloatingActionButton(
-      tooltip: 'Your profile',
-      elevation: 8.0,
-      backgroundColor: _backgroundColor,
-      onPressed: () {},
-      child: CircleAvatar(
-        radius: 26.0,
+    return Padding(
+      padding: EdgeInsets.only(right: 8.0),
+      child: FloatingActionButton(
+        tooltip: 'Your profile',
         backgroundColor: _backgroundColor,
-        backgroundImage: NetworkImage(
-            'https://images.unsplash.com/photo-1602773890240-87ce74fc752e?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80'),
+        mini: true,
+        onPressed: () {},
+        child: CircleAvatar(
+          radius: 18.0,
+          backgroundColor: _backgroundColor,
+          backgroundImage: (_data.userAvatarImage != null &&
+                  _data.userAvatarImage.isNotEmpty)
+              ? NetworkImage(_data.userAvatarImage)
+              : AssetImage('assets/image/no_avatar.png'),
+        ),
       ),
     );
   }
